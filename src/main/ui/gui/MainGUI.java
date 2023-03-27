f2815ce1b856be84a220bdbc97af70c4e64f6558
chenkai <y7v1z@ugrad.cs.ubc.ca>
2023-03-27 02:01:15 -0700

    Phase 3 - learning JFrame

---
+++ src/main/ui/gui/MainGUI.java
@@ -0,0 +1,10 @@
+package ui.gui;
+
+import java.io.FileNotFoundException;
+
+//Run the GUI from here.
+public class MainGUI {
+    public static void main(String[] args) {
+        new SplitAppGUI();
+    }
+}
---
+++ src/main/ui/gui/SplitAppGUI.java
@@ -0,0 +1,21 @@
+package ui.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SplitAppGUI extends JFrame {
+    public SplitAppGUI() {
+        super("Split The Bill!");
+        setVisible(true);
+        setSize(400,400);
+        setLayout(new FlowLayout()); //default CardLayout. FlowLayout, GridLayout, Null
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel l = new JLabel("Hello!!");
+        JLabel l1 = new JLabel("Welcome here");
+        add(l);
+        add(l1);
+
+
+    }
+}
