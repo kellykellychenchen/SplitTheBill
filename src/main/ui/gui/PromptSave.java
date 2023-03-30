b4b725049cdee6189968ca516e78afe2c6b0cbef
kellychen <chenkai@students.cs.ubc.ca>
2023-03-30 13:08:12 -0700

    Phase 3 - progress2

---
+++ src/main/ui/gui/PromptSave.java
@@ -0,0 +1,37 @@
+package ui.gui;
+
+import model.BillBook;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PromptSave extends JFrame implements ActionListener {
+    JLabel l1 = new JLabel("Would you like to save your data?");
+    JButton b1 = new JButton("Yes");
+    JButton b2 = new JButton("No");
+    SplitAppGUI app;
+
+    public PromptSave(SplitAppGUI app) {
+        setVisible(true);
+        setSize(400, 400);
+        setLayout(new FlowLayout()); //default CardLayout. FlowLayout, GridLayout, Null
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.app = app;
+
+        add(l1);
+        add(b1);
+        add(b2);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            this.app.saveBillBook();
+        }
+        dispose();
+    }
+}
--- src/main/ui/gui/SplitAppGUI.java
+++ src/main/ui/gui/SplitAppGUI.java
@@ -9,6 +9,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class SplitAppGUI extends JFrame implements ActionListener {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private List<JButton> buttons;
+    private List<JLabel> labels;
     public SplitAppGUI() {
         super("Split The Bill!");
 
@@ -25,6 +28,7 @@ public class SplitAppGUI extends JFrame implements ActionListener {
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
         buttons = new ArrayList<>();
+        labels = new ArrayList<>();
 
         setVisible(true);
         setSize(400,400);
@@ -32,6 +36,7 @@ public class SplitAppGUI extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel l0 = new JLabel("How would you like to start?");
+        labels.add(l0);
         JButton b0 = new JButton("Create a new event");
         buttons.add(b0);
         JButton b1 = new JButton("Select from existing events");
@@ -42,6 +47,8 @@ public class SplitAppGUI extends JFrame implements ActionListener {
         buttons.add(b3);
         JButton b4 = new JButton("quit");
         buttons.add(b4);
+        JLabel l1 = new JLabel();
+        labels.add(l1);
 
         add(l0);
         add(b0);
@@ -49,6 +56,7 @@ public class SplitAppGUI extends JFrame implements ActionListener {
         add(b2);
         add(b3);
         add(b4);
+        add(l1);
 
         b0.addActionListener(this);
         b1.addActionListener(this);
@@ -67,13 +75,36 @@ public class SplitAppGUI extends JFrame implements ActionListener {
             new SelectEvent(this.billBook);
         }
         if (e.getSource() == buttons.get(2)) {
-
+            saveBillBook();
         }
         if (e.getSource() == buttons.get(3)) {
-
+            loadBillBook();
         }
         if (e.getSource() == buttons.get(4)) {
-
+            new PromptSave(this);
+            dispose();
         }
     }
+
+    public void saveBillBook() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(billBook);
+            jsonWriter.close();
+            labels.get(1).setText("Saved " + billBook.getName() + " to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            labels.get(1).setText("Unable to write to file: " + JSON_STORE);
+        }
+    }
+
+    private void loadBillBook() {
+        try {
+            this.billBook = jsonReader.read();
+            labels.get(1).setText("Loaded " + billBook.getName() + " from " + JSON_STORE);
+        } catch (IOException e) {
+            labels.get(1).setText("Unable to read from file: " + JSON_STORE);
+        }
+    }
+
+
 }
